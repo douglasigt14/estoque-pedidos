a8f300cb4465039fe732477a68b375c38619b6ce
Douglas Rocha <douglas.rocha@greenn.com.br>
2024-06-22 15:47:38 -0300

    Primeiro Commit

---
+++ database/migrations/2024_06_22_175123_create_itens_pedido_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItensPedidoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itens_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained()->onDelete('cascade');
+            $table->foreignId('produto_id')->constrained()->onDelete('cascade');
+            $table->integer('quantidade');
+            $table->decimal('preco', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_pedido');
+    }
+}
+
