f0630d183e1e96a98de006fea9c96ae6ad8b7a22
Douglas Rocha <douglas.rocha@greenn.com.br>
2024-06-22 22:15:50 -0300

    template

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -4,47 +4,48 @@
 <link rel="stylesheet" href="{{ asset('css/product.css') }}">
 @endpush
 
+@section('title', 'Lista de Produtos')
+
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
+    <div class="container d-flex flex-column w-100">
                 <div class="row d-flex justify-content-between">
-                    <div> <h1>Lista de Produtos</h1></div>
+                    <div> </div>
                     <div><a href="{{ route('produtos.create') }}" class="btn btn-primary mb-3">+</a></div>
 
                 </div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Quantidade</th>
-                            <th>Preço</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($produtos as $produto)
+                <div class="d-flex w-100">
+                    <table class="table w-100">
+                        <thead>
                             <tr>
-                                <td>{{ $produto->id }}</td>
-                                <td>{{ $produto->nome }}</td>
-                                <td>{{ $produto->descricao }}</td>
-                                <td>{{ $produto->quantidade }}</td>
-                                <td>{{ $produto->preco }}</td>
-                                <td>
-                                    <a href="{{ route('produtos.edit', $produto->id) }}" class="btn btn-primary btn-sm">Editar</a>
-                                    <form action="{{ route('produtos.destroy', $produto->id) }}" method="POST" class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Tem certeza que deseja excluir este produto?')">Excluir</button>
-                                    </form>
-                                </td>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Quantidade</th>
+                                <th>Preço</th>
+                                <th>Ações</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                        </thead>
+                        <tbody>
+                            @foreach ($produtos as $produto)
+                                <tr>
+                                    <td>{{ $produto->id }}</td>
+                                    <td>{{ $produto->nome }}</td>
+                                    <td>{{ $produto->descricao }}</td>
+                                    <td>{{ $produto->quantidade }}</td>
+                                    <td>{{ $produto->preco }}</td>
+                                    <td>
+                                        <a href="{{ route('produtos.edit', $produto->id) }}" class="btn btn-primary btn-sm">Editar</a>
+                                        <form action="{{ route('produtos.destroy', $produto->id) }}" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Tem certeza que deseja excluir este produto?')">Excluir</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+               
     </div>
 @endsection
