b16ff8ae9dace0638782f72a91c7351c0c3f1d3b
Douglas Rocha <douglas.rocha@greenn.com.br>
2024-06-24 16:39:22 -0300

    Produtos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Sistema de Estoque">
+    <meta name="author" content="Douglas">
+    <link rel="manifest" href="/manifest.json">
+
 
     <title>Estoque</title>
 
