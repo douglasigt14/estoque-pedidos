db85e4442d0de70c79c2402f76a864ab526de34e
Douglas Rocha <douglas.rocha@greenn.com.br>
2024-06-26 18:02:25 -0300

    Qtd

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -109,39 +109,5 @@
 
 @push('scripts')
 <script src="{{ asset('js/uteis.js') }}"> </script>
-<script>
-  function openModal(produto = null) {
-    const modal = $('#modal');
-    const form = $('#formInserirEditar');
-    const methodField = $('#methodField');
-
-    if (produto) {
-      // Preencher o formulário com os valores do produto para edição
-      $('#modalLabel').text('Editar Produto');
-      form.attr('action', `/produtos/${produto.id}`);
-      methodField.html('@method('PUT')');
-      
-      $('#nome').val(produto.nome);
-      $('#descricao').val(produto.descricao);
-      $('#preco').val(produto.preco);
-      $('#preco_revenda').val(produto.preco_revenda);
-      $('#quantidade').val(produto.quantidade);
-      $('#cor').val(produto.cor);
-    } else {
-      // Limpar o formulário para inserção de um novo produto
-      $('#modalLabel').text('Inserir Novo Produto');
-      form.attr('action', '/produtos');
-      methodField.html('');
-      
-      $('#nome').val('');
-      $('#descricao').val('');
-      $('#preco').val('');
-      $('#preco_revenda').val('');
-      $('#quantidade').val(1);
-      $('#cor').val('');
-    }
-
-    modal.modal('show');
-  }
-</script>
+<script src="{{ asset('js/produtos.js') }}"> </script>
 @endpush
